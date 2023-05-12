19e0a961d94234cb11416fbe02da415df0636501
Taye <tayeshobajo@gmail.com>
2023-05-12 20:03:28 +0530

    May 12 Updates Header Fixess

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./header.css";
 import { MenuItems } from "./MenuItems";
 import { HiOutlineMenuAlt1 } from "react-icons/hi";
