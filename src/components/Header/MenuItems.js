ca4369447d9e6c593b66ba0d2e767fd2aa8ddea1
Elite Web Solutions <111870642+elitewebsolutions@users.noreply.github.com>
2024-05-08 15:21:35 +0530

    Update MenuItems.js

--- src/components/Header/MenuItems.js
+++ src/components/Header/MenuItems.js
@@ -1,76 +1,76 @@
 export const MenuItems = [
-    {
-        title: "Home",
-        url: "/",
+  {
+    title: "Home",
+    url: "/",
+    cName: "nav-links",
+    subMenu: false,
+  },
+  {
+    title: "Services",
+    url: "/services/",
+    cName: "nav-links",
+    subMenu: true,
+    sMenu: [
+      {
+        title: "WordPress Development",
+        url: "/services/wordpress-development/",
         cName: "nav-links",
-        subMenu:false
-
-    },
-    {
-        title: "Services",
-        url: "/services/",
+      },
+      {
+        title: "Learndash LMS Development",
+        url: "/services/learndash-development/",
         cName: "nav-links",
-        subMenu:true,
-        sMenu: [
-          {
-            title: "WordPress Development",
-            url: "/services/wordpress-development/",
-            cName: "nav-links",
-          },
-          {
-            title: "Learndash LMS Development",
-            url: "/services/learndash-development/",
-            cName: "nav-links",
-          },
-          {
-            title: "Web Development",
-            url: "/services/web-development/",
-            cName: "nav-links",
-          },
-          {
-            title: "UI/UX Development",
-            url: "/services/ui-ux-development/",
-            cName: "nav-links",
-          },
-          {
-            title: "Web Maintenance",
-            url: "/services/web-maintenance/",
-            cName: "nav-links",
-          },
-          {
-            title: "Search Engine Optimization",
-            url: "/services/seo/",
-            cName: "nav-links",
-          },
-          {
-            title: "Web Hosting",
-            url: "/services/web-hosting/",
-            cName: "nav-links",
-          }
-        ]
-    },
-    {
-      title: "Portfolio",
-      url: "/portfolio/",
-      cName: "nav-links",
-      subMenu:false
-    },
-    {
-        title: "About us",
-        url: "/about-us/",
+      },
+      {
+        title: "Web Development",
+        url: "/services/web-development/",
         cName: "nav-links",
-        subMenu:false
-    },
-    {
-        title: "Pricing",
-        url: "/pricing/",
+      },
+      {
+        title: "UI/UX Development",
+        url: "/services/ui-ux-development/",
         cName: "nav-links",
-        subMenu:false
-    },
-    {
-        title: "Contact",
-        url: "/contact/",
+      },
+      {
+        title: "Web Maintenance",
+        url: "/services/web-maintenance/",
         cName: "nav-links",
-        subMenu:false
-    },
+      },
+      {
+        title: "Search Engine Optimization",
+        url: "/services/seo/",
+        cName: "nav-links",
+      },
+      {
+        title: "Web Hosting",
+        url: "/services/web-hosting/",
+        cName: "nav-links",
+      },
+    ],
+  },
+  {
+    title: "Portfolio",
+    url: "/portfolio/",
+    cName: "nav-links",
+    subMenu: false,
+  },
+  {
+    title: "About us",
+    url: "/about-us/",
+    cName: "nav-links",
+    subMenu: false,
+  },
+  {
+    title: "Pricing",
+    url: "/pricing/",
+    cName: "nav-links",
+    subMenu: false,
+  },
+  {
+    title: "Contact",
+    url: "/contact/",
+    cName: "nav-links",
+    subMenu: false,
+    onMobile: true,
+  },
 ];
