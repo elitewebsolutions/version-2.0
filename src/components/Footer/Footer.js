c8f072b65f284ba1908d6c5955361d4d1e98e677
Elite Web Solutions <111870642+elitewebsolutions@users.noreply.github.com>
2024-08-08 14:58:22 +0530

    Update Address Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -125,7 +125,7 @@ function Footer() {
                       </a>
                     </p>
                     <p className="location-footer">
-                      <a href="https://www.google.com/maps/dir//Huntington+Dr,+Murfreesboro,+TN+37130/@35.8632245,-86.3779161,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x8864077f9c19aa03:0x3c7dc50919704dd1!2m2!1d-86.3779161!2d35.8632245" target="_blank" rel="noopener noreferrer">
+                      <a id="custom-link-dissable" href="https://www.google.com/maps/dir//Huntington+Dr,+Murfreesboro,+TN+37130/@35.8632245,-86.3779161,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x8864077f9c19aa03:0x3c7dc50919704dd1!2m2!1d-86.3779161!2d35.8632245" target="_blank" rel="noopener noreferrer">
                       2615 Medical Center Parkway Suite 1560, Murfreesboro Tennessee, US - 37129</a>
                     </p>
                     <p className="location-footer">
